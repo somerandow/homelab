6ed2087bd8c5a4f77f4c9aace374f89cc8e1a7c7
Thomas John Randowski <wojoinc@pm.me>
2026-04-19 22:27:13 -0500

    final refactor of plex app

---
+++ scripts/render_app.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function move_kustomization() {
+    cp "argocd/app/$1/base/kustomization.yaml" "argocd/app/$1/base/kustomization.yaml.bak"
+}
+
+function restore_kustomization() {
+    cp "argocd/app/$1/base/kustomization.yaml.bak" "argocd/app/$1/base/kustomization.yaml" || exit
+    rm "argocd/app/$1/base/kustomization.yaml.bak"
+}
+
+function missing_component() {
+    echo "WARN: missing/invalid component: $1, real path: $(realpath $1)" >&2
+}
+
+function add_components() {
+    olddir=$(pwd)
+    cd "argocd/app/$1/base" || exit
+    # TODO automatically determine env etc.
+    kustomize edit add component ../../../flavor/talos/common 2>/dev/null || missing_component ../../../flavor/talos/common
+    kustomize edit add component "../components/flavor/talos" 2>/dev/null || missing_component "../components/flavor/talos"
+    kustomize edit add component ../../../env/prod/common 2>/dev/null || missing_component ../../../env/prod/common
+    kustomize edit add component "../components/env/prod" 2>/dev/null || missing_component "../components/env/prod"
+    kustomize edit add component "../../../clusters/$2/common" 2>/dev/null || missing_component "../../../clusters/$2/common"
+    kustomize edit add component "../components/clusters/$2" 2>/dev/null || missing_component "../components/clusters/$2"
+    cd "$olddir" || exit
+}
+
+function build_kustomization() {
+    if [ -t 1 ]; then
+      read -rsp "Review warnings, then press <Enter> to render"
+      kustomize build "argocd/app/$1/base" | less
+    else
+      kustomize build "argocd/app/$1/base"
+    fi
+}
+
+app=$1
+cluster=$2
+
+echo -e "rendering:\napp: $app\ncluster: $cluster" >&2
+
+move_kustomization "$app"
+add_components "$app" "$cluster"
+build_kustomization "$app"
+restore_kustomization "$app"
